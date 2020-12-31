c2eef4a610c0ca5e540135d195eb81e12f8615bb
Chutikran <005774@cdg.co.th>
2020-12-31 22:49:03 +0700

    update to 11

--- src/app/page-one/page-one.component.spec.ts
+++ src/app/page-one/page-one.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { PageOneComponent } from './page-one.component';
 
@@ -6,7 +6,7 @@ describe('PageOneComponent', () => {
   let component: PageOneComponent;
   let fixture: ComponentFixture<PageOneComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ PageOneComponent ]
     })
